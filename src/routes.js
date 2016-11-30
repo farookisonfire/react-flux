6ca78b79f4a38d025815428bb015f999224f940d
Farook Khan <farook.khan@gmail.com>
2016-11-29 23:05:23 -0800

    setting up react-router

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,15 @@ const React = require('react');
 const Router = require('react-router');
 const DefaultRoute = Router.DefaultRoute;
 const Route = Router.Route;
+const NotFoundRoute = Router.NotFoundRoute;
+
 
 var routes = (
     <Route name="app" path="/" handler={require('./components/app')}>
       <DefaultRoute handler={require('./components/homepage')} />
       <Route name="authors" handler={require('./components/authors/authorpage')}/>
       <Route name="about" handler={require('./components/about/aboutpage')} />
+      <NotFoundRoute handler={require('./components/notFoundPage')} />
     </Route>
 );
 
